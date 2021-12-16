050d4a0c82f18df1b8cf45ddfcdefbf4b1e9ec63
Fatemeh Saedi <fatemehsd74@gmail.com>
2021-12-16 04:03:24 +0330

    First commit

---
+++ packages/app/metro.config.js
@@ -0,0 +1,32 @@
+/**
+ * Metro configuration for React Native
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+const path=require('path')
+const extraNodeModules={
+  shared:path.resolve(__dirname+'/../shared')
+}
+
+const watchFolders=[path.resolve(__dirname+'/../shared')]
+
+module.exports = {
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: true,
+      },
+    }),
+  },
+  resolver:{
+    extraNodeModules:new Proxy(extraNodeModules,{
+      get:(target,name)=>
+      name in target
+      ? target[name]
+      : path.join(process.cwd(),`node_modules/${name}`)
+    })
+  },
+  watchFolders
+};
---
+++ packages/shared/index.js
@@ -0,0 +1,3 @@
+const shared ="Hey...!"
+
+export default shared;
